0f5c737c4979d90889488f93abccfdbea857f5cd
Matt Wilde <matt@zenpayroll.com>
2014-12-02 11:20:27 -0800

    Remove hard exclusion on Konacha and factor it out into a predicate.
    Closes #1

--- lib/assets_precompile_enforcer.rb
+++ lib/assets_precompile_enforcer.rb
@@ -1,6 +1,2 @@
 require "assets_precompile_enforcer/version"
-
-# Needs to be disabled for Konacha
-unless defined?(Konacha)
-  require "assets_precompile_enforcer/sprockets/helpers/rails_helper"
-end
+require "assets_precompile_enforcer/sprockets/helpers/rails_helper"
